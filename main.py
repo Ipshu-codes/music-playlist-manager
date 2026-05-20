4cd497f68fb163760a7a6b35936ec7f3b7bce651
gugu-codes <ipshu.upreti@gmail.com>
2026-05-20 20:40:16 +0545

    Make multiple playlists and decide which one to play

--- main.py
+++ main.py
@@ -1,40 +1,79 @@
 from song import Song
 from playlist import Playlist
 
-playlist = Playlist("My Playlist")
+playlists = {}  
+
+current_playlist = None
 
 while True:
-    print("\n--- MUSIC PLAYLIST ---")
-    print("1. Add Song")
-    print("2. Show Playlist")
-    print("3. Remove Song")
-    print("4. Save Playlist")
-    print("5. Exit")
+    print("\n--- PLAYLIST MANAGER ---")
+    print("1. Create Playlist")
+    print("2. Select Playlist")
+    print("3. Add Song")
+    print("4. Remove Song")
+    print("5. Show Playlist")
+    print("6. Show All Playlists")
+    print("7. Exit")
 
     choice = input("Enter choice: ")
 
+    
     if choice == "1":
-        title = input("Song title: ")
-        artist = input("Artist: ")
+        name = input("Enter playlist name: ")
 
-        song = Song(title, artist)
-        playlist.add_song(song)
+        if name in playlists:
+            print("Playlist already exists!")
+        else:
+            playlists[name] = Playlist(name)
+            print(f"Playlist '{name}' created")
 
+    
     elif choice == "2":
-        playlist.show_playlist()
+        name = input("Enter playlist name to select: ")
+
+        if name in playlists:
+            current_playlist = playlists[name]
+            print(f"Selected playlist: {name}")
+        else:
+            print("Playlist not found")
+
 
     elif choice == "3":
-        title = input("Enter song title to remove: ")
-        playlist.remove_song(title)
+        if current_playlist is None:
+            print("Select a playlist first!")
+        else:
+            title = input("Song title: ")
+            artist = input("Artist: ")
 
+            song = Song(title, artist)
+            current_playlist.add_song(song)
+
+    
     elif choice == "4":
-        with open("playlist.txt", "w") as file:
-            for song in playlist.songs:
-                file.write(f"{song.title} - {song.artist}\n")
+        if current_playlist is None:
+            print("Select a playlist first!")
+        else:
+            title = input("Song title to remove: ")
+            current_playlist.remove_song(title)
 
-        print("Playlist saved to playlist.txt")
-
+    
     elif choice == "5":
+        if current_playlist is None:
+            print("Select a playlist first!")
+        else:
+            current_playlist.show_playlist()
+
+    
+    elif choice == "6":
+        if not playlists:
+            print("No playlists created")
+        else:
+            print("\nAll Playlists:")
+            for name in playlists:
+                print("-", name)
+
+    
+    elif choice == "7":
         print("Have a blast!")
         break
 
